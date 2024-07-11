86ff8072fba4fae9d41cd7e927a155d9ca3f621d
Tor Shepherd <tor.aksel.shepherd@gmail.com>
2024-07-10 23:06:20 -0400

    stop trying to make fetch happen!

--- antics/TODO/static_fetch.cpp
+++ antics/TODO/static_fetch.cpp
@@ -10,10 +10,14 @@
 //
 // DESKTOP-0M124BO
 
+#if __has_embed("/etc/hostname")
 static constexpr const char raw_hostname[] = {
-#embed "/etc/hostname" suffix(, )
+#  embed "/etc/hostname" suffix(, )
     0};
 static constexpr std::string_view hostname{raw_hostname};
+#else
+static constexpr std::string_view hostname = "(unknown hostname)";
+#endif
 
 // /etc/lsb-release:
 //
@@ -22,8 +26,9 @@ static constexpr std::string_view hostname{raw_hostname};
 // DISTRIB_CODENAME=jammy
 // DISTRIB_DESCRIPTION="Ubuntu 22.04.3 LTS"
 
+#if __has_embed("/etc/lsb-release")
 static constexpr const char raw_lsb_release[] = {
-#embed "/etc/lsb-release" suffix(, )
+#  embed "/etc/lsb-release" suffix(, )
     0};
 static constexpr std::string_view lsb_release{raw_lsb_release};
 static constexpr std::string_view last_line_of_lsb_release =
@@ -33,32 +38,84 @@ static constexpr std::string_view distrib_description =
         last_line_of_lsb_release.find('\"') + 1,
         last_line_of_lsb_release.find_last_of('\"') -
             last_line_of_lsb_release.find('\"') - 1);
+#else
+static constexpr std::string_view distrib_description =
+    "(unknown distribution)";
+#endif
+
+static constexpr std::string_view os_class =
+#ifdef _WIN64
+    "Windows 64-bit"
+#elif defined(_WIN32)
+    "Windows 32-bit"
+#elif defined(__APPLE__)
+    "Apple"
+#elif defined(__MACH__)
+    "Apple"
+#elif defined(__ANDROID__)
+    "Android"
+#elif defined(__linux__)
+    "Linux"
+#elif defined(__sun)
+    "Solaris"
+#elif defined(__hpux)
+    "HP UX"
+#elif defined(__DragonFly__)
+    "DragonFly BSD"
+#elif defined(__FreeBSD__)
+    "FreeBSD"
+#elif defined(__NetBSD__)
+    "NetBSD"
+#elif defined(__OpenBSD__)
+    "OpenBSD"
+#elif defined(BSD)
+    "BSD"
+#elif defined(_POSIX_VERSION)
+    "POSIX-based"
+#elif defined(__unix__)
+    "Unix-based OS"
+#endif
+    ;
 
 // /etc/subuid:
 //
 // tor:XXX:XXX
 
+#if __has_embed("/etc/subuid")
 static constexpr const char raw_subuid[] = {
-#embed "/etc/subuid" suffix(, )
+#  embed "/etc/subuid" suffix(, )
     0};
 static constexpr std::string_view subuid{raw_subuid};
 static constexpr std::string_view username = subuid.substr(0, subuid.find(':'));
+#else
+static constexpr std::string_view username = "(unknown user)";
+#endif
 
 // /etc/passwd
 //
 // tor:x:1000:1000:,,,:/home/tor:/usr/bin/zsh
 // USERNAME:PASS:UID:GID:UID INFO:HOME:SHELL
 
+#if __has_embed("/etc/passwd")
 static constexpr const char raw_passwd[] = {
-#embed "/etc/passwd" suffix(, )
+#  embed "/etc/passwd" suffix(, )
     0};
 static constexpr std::string_view passwd{raw_passwd};
 static constexpr auto passwd_offset_for_subuid = passwd.find(username);
-static constexpr std::string_view passwd_entry_for_subuid = passwd.substr(
-    passwd_offset_for_subuid,
-    passwd.find('\n', passwd_offset_for_subuid) - passwd_offset_for_subuid);
-static constexpr std::string_view subuid_shell = passwd_entry_for_subuid.substr(
-    passwd_entry_for_subuid.find_last_of(':') + 1);
+static constexpr std::string_view passwd_entry_for_subuid =
+    passwd_offset_for_subuid == std::string_view::npos
+        ? ""
+        : passwd.substr(passwd_offset_for_subuid,
+                        passwd.find('\n', passwd_offset_for_subuid) -
+                            passwd_offset_for_subuid);
+static constexpr std::string_view subuid_shell =
+    passwd_entry_for_subuid == ""
+        ? "(unknown shell)"
+        : passwd_entry_for_subuid.substr(
+              passwd_entry_for_subuid.find_last_of(':') + 1);
+#else
+static constexpr std::string_view subuid_shell = "(unknown shell)";
+#endif
 
 constexpr bool done = false;
 static_assert(
@@ -69,14 +126,14 @@ done
     [] -> std::string {
       char buffer[4096]{};
       auto running = buffer;
-      running =
-          fmt::format_to(running, FMT_COMPILE("\n\n{}@{}"), username, hostname);
+      running = fmt::format_to(running, FMT_COMPILE("\n\n{}@{}\n"), username,
+                               hostname);
       const ptrdiff_t first_line_length = running - buffer;
       running = fmt::format_to(running, FMT_COMPILE("{0:-^{1}}\n"), "",
                                first_line_length - 3 /* The newlines */);
+      running = fmt::format_to(running, FMT_COMPILE("OS: {} [{}]\n"),
+                               distrib_description, os_class);
       running =
-          fmt::format_to(running, FMT_COMPILE("OS: {}\n"), distrib_description);
-      running =
-          fmt::format_to(running, FMT_COMPILE("Shell: {}\n"), subuid_shell);
+          fmt::format_to(running, FMT_COMPILE("Shell: {}\n\n"), subuid_shell);
       return buffer;
     }());
